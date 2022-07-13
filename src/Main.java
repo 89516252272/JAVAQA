a6218600df0fc748f5f56d1106cea6e637087171
Asmik <hasohovsepyan@bk.ru>
2022-07-13 17:04:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int ticketPrice = 15000;
+          int miles = ticketPrice / 20;
+               System.out.println("Вам начислят " + miles + " миль.");
+
+    }
+
+}
